409cb235cb94012892f8b431aeda95ce0f7e8939
crisgtk <63614285+crisgtk@users.noreply.github.com>
2025-03-28 21:13:28 -0300

    resolve footer (#6)

--- src/components/common/default-footer/ContactMeta.js
+++ src/components/common/default-footer/ContactMeta.js
@@ -1,3 +1,4 @@
+import { formatPhoneNumber } from "@/utilis/formatPhoneNumber";
 import React from "react";
 
 const ContactMeta = () => {
@@ -6,11 +7,19 @@ const ContactMeta = () => {
       text: "Concepción",
       info: "Barros Arana 1098 Torre Mall del Centro Oficina #1807.",
       link: "#", // Empty link value for the first object
+      tel: "Cel +56 9 2383 0830"
     },
     {
       text: "Chillán",
       info: "Av Libertad 845 Edificio Don Alfonso Oficina #303",
       link: "#",
+      tel: "Cel + 56 9 6676 4574"
+    },
+    {
+      text: "Los Angeles",
+      info: "José Manso de Velazco 221, Oficina #1102",
+      link: "#",
+       tel: "Cel + 56 9 7306 2812"
     },
     {
       text: "Contactanos",
@@ -21,20 +30,27 @@ const ContactMeta = () => {
 
   return (
     <div className="row mb-4 mb-lg-5">
-      {contactInfoData.map((contact, index) => (
-        <div className="contact-info mb25" key={index}>
-          <p className="info-title mb5">{contact.text}</p>
-          {contact.link.startsWith("mailto:") ? (
-            <h6 className="info-mail">
-              <a href={contact.link}>{contact.info}</a>
-            </h6>
-          ) : (
-            <h6 className="info-phone">
-              <a href={contact.link}>{contact.info}</a>
-            </h6>
-          )}
-        </div>
-      ))}
+    {contactInfoData.map((contact, index) => (
+         <div className="contact-info mb25" key={index}>
+           <p className="info-title mb5">{contact.text}</p>
+           {contact.link.startsWith("mailto:") ? (
+             <>
+             <h6 className="info-mail">
+               <a href={contact.link}>{contact.info}</a>
+             </h6>
+           </>
+           ) : (
+             <>
+             <h6 className="info-phone">
+               <a href="#">{contact.info}</a>
+             </h6>
+               <h6 className="info-phone">
+               <a href={`tel:${formatPhoneNumber(contact.tel)}`}>{contact.tel}</a>
+             </h6>
+             </>
+           )}
+         </div>
+       ))}
     </div>
   );
 };
--- src/components/home/home-v5/footer/MenuWidget.js
+++ src/components/home/home-v5/footer/MenuWidget.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const MenuWidget = () => {
@@ -37,9 +38,10 @@ const MenuWidget = () => {
               <h6 className="text-white mb25">{section.title}</h6>
               <ul className="ps-0">
                 {section.links.map((link, linkIndex) => (
-                  <li key={linkIndex}>
-                    <a href={link.href}>{link.label}</a>
-                  </li>
+                  <Link href={link.href}
+                  >
+                     {link.label}
+                  </Link>
                 ))}
               </ul>
             </div>
