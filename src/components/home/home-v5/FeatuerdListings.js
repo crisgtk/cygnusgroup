8c363d1b7e40f5d5b595f67a7c7f76cd3f68f80b
crisgtk <63614285+crisgtk@users.noreply.github.com>
2025-07-14 08:36:29 -0400

    Update FeatuerdListings.js (#21)
    resolve link

--- src/components/home/home-v5/FeatuerdListings.js
+++ src/components/home/home-v5/FeatuerdListings.js
@@ -233,7 +233,7 @@ const FeaturedListings = () => {
                       </ul> */}
               </div>
               <h6 className="list-title">
-                <Link href={`/single-v3/${listing.id}`}>{listing.title}</Link>
+                <Link href={`/single-v1/${listing.id}`}>{listing.title}</Link>
               </h6>
               <p className="list-text">{listing.location}</p>
               <div className="list-meta d-flex align-items-center">
